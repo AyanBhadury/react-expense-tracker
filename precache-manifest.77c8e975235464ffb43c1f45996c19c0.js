8af5f87872c3da307a5baf2879b5ee8d428279e6
ayanbhaduryfc <ayon.bony@gmail.com>
2020-04-19 16:51:23 +0530

    Updates

---
+++ precache-manifest.77c8e975235464ffb43c1f45996c19c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1cc236229932667654f80b1002a212",
+    "url": "/AyanBhadury/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "9ff150f06cb78365f0ae",
+    "url": "/AyanBhadury/react-expense-tracker/static/css/main.2c78bf4d.chunk.css"
+  },
+  {
+    "revision": "92110e239831f0bfb7b9",
+    "url": "/AyanBhadury/react-expense-tracker/static/js/2.c689ddfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AyanBhadury/react-expense-tracker/static/js/2.c689ddfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff150f06cb78365f0ae",
+    "url": "/AyanBhadury/react-expense-tracker/static/js/main.0473a3d5.chunk.js"
+  },
+  {
+    "revision": "8af55b9b8cf12baefa78",
+    "url": "/AyanBhadury/react-expense-tracker/static/js/runtime-main.caae1d9b.js"
+  }
+]);
